a894797c7d4c0d0d51ab3538c662df3974d296c9
Chang Che <changche@Changs-MacBook-Pro.local>
2023-11-09 11:33:40 -0500

    Nov9

--- interview/Solution.java
+++ interview/Solution.java
@@ -18,6 +18,104 @@ public class Solution {
 		result.stream().forEach(s1 -> {s1.stream().forEach(System.out::print); System.out.println();});
 	}
 	
+	public void solveSudoku(char[][] board) {
+		solve(board);
+	}
+	
+	public boolean solve(char[][] board) {
+		for(int row = 0; row < 9; row++) {
+			for(int col = 0; col < 9; col++) {
+				if(board[row][col] == '.') {
+					for(char num = '1'; num <= '9'; num++) {
+						if(isValid(board, row, col, num)) {
+							board[row][col] = num;
+							if(solve(board)) {
+								return true;
+							}else {
+								board[row][col] = '.';
+							}
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private boolean isValid(char[][] board, int row, int col, char ch) {
+		for(int i = 0; i < 9; i++) {
+			if(board[row][i] == ch) return false;
+			if(board[i][col] == ch) return false;
+		}
+		int r = row - row % 3;
+		int c = col - col % 3;
+		for(int i = r; i < r + 3; i++) {
+			for(int j = c; j < c + 3; j++) {
+				if(board[i][j] == ch) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean isValidSudoku(char[][] board) {
+		for(int row = 0; row < 9; row++) {
+			if(!isValidRow(board, row)) {
+				return false;
+			}
+		}
+		for(int col = 0; col < 9; col++) {
+			if(!isValidCol(board, col)) {
+				return false;
+			}
+		}
+		for(int row = 0; row < 9; row += 3) {
+			for(int col = 0; col < 9; col += 3) {
+				if(!isValidSubGrid(board, row, col)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private boolean isValidSubGrid(char[][] board, int row, int col) {
+		boolean[] seen = new boolean[9];
+		for(int i = row; i < row + 3; i++) {
+			for(int j = col; j < col + 3; j++) {
+				if(seen[board[row][col] - '1']) {
+					return false;
+				}
+				seen[board[row][col] - '1'] = true;
+			}
+		}
+		return true;
+	}
+	
+	private boolean isValidCol(char[][] board, int col) {
+		boolean[] seen = new boolean[9];
+		for(int row = 0; row < 9; row++) {
+			if(seen[board[row][col] - '1']) {
+				return false;
+			}
+			seen[board[row][col] - '1'] = true;
+		}
+		return true;
+	}
+	
+	private boolean isValidRow(char[][] board, int row) {
+		boolean[] seen = new boolean[9];
+		for(int col = 0; col < 9; col++) {
+			if(seen[board[row][col] - '1']) {
+				return false;
+			}
+			seen[board[row][col] - '1'] = true;
+		}
+		return true;
+	}
+	
 	
 	static class ListNode{
 		int value;
