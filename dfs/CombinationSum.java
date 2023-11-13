ba44b11f94f66c9a618b37c5f733c142a3da0b38
Chang Che <changche@Changs-MacBook-Pro.local>
2023-11-13 17:53:22 -0500

    Nov13

--- dfs/CombinationSum.java
+++ dfs/CombinationSum.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// every element can be used multiple times
 public class CombinationSum {
 
 	public static void main(String[] args) {
