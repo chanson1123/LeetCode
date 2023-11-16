7a9d03b3a34c94df9b5c9b5ae8924575910d7d60
Chang Che <changche@Changs-MacBook-Pro.local>
2023-11-16 17:32:24 -0500

    Nov16

--- dp/TrappingRainWater.java
+++ dp/TrappingRainWater.java
@@ -5,6 +5,7 @@ public class TrappingRainWater {
 	
 	
 	public int trap(int[] height) {
+		//left[]左边能support的最大高度
 		int max1 = 0;
 		int left[] = new int[height.length];
 		for (int i = 0; i < height.length; i++) {
@@ -13,7 +14,7 @@ public class TrappingRainWater {
 			}
 			left[i] = max1;
 		}
-
+		//right[]右边能support的最大高度
 		int max2 = 0;
 		int right[] = new int[height.length];
 		for (int i = height.length - 1; i >= 0; i--) {
@@ -22,7 +23,7 @@ public class TrappingRainWater {
 			}
 			right[i] = max2;
 		}
-
+		//某一个节点短板效应，左右谁短谁决定高度，再减去自身原本高度，就是蓄水量
 		int trap = 0;
 		for (int i = 0; i < height.length; i++) {
 			trap += Math.min(left[i], right[i]) - height[i];
